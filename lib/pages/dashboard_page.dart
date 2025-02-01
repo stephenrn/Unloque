54b5532721ca1d4f4b316888a480b3bc4b19de41
Stephen Villeta <139435563+stephenrn@users.noreply.github.com>
2025-02-01 18:02:41 +0000

    change sample image in dashboard

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -14,7 +14,7 @@ class DashboardPage extends StatelessWidget {
         date: '2023-10-01',
         headline: 'EduPH Opportunity 2.0',
         backgroundImage:
-            'https://www.borgenmagazine.com/wp-content/uploads/2024/08/8644294742_96b35cd70a_k.jpg',
+            'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
         route: '/sampleartceducation',
       ),
       SliderItem(
