fc433699b1e2c8dc177800dbfb30b4c965296a0e
[Aerron Gumarao] <[aerronsandergumarao@gmail.com]>
2025-02-02 14:33:21 +0800

    minor revision

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:unloque/pages/login_page.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
