4c8763b2c5a47dd7a3327584f2d70ff90184750d
Stephen Raine Villeta <stephenrainevilleta16@gmail.com>
2025-02-09 22:38:02 +0800

    const google_button

--- lib/components/google_button.dart
+++ lib/components/google_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class GoogleButton extends StatelessWidget {
+  const GoogleButton({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
